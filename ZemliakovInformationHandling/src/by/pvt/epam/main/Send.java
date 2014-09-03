8a3aec51b5930e3953eedf3401bd50c1ab75c462
Nelegalos <zemliakov@gmail.com>
2014-09-03 19:00:18 +0300

    epamchange

---
+++ ZemliakovInformationHandling/src/by/pvt/epam/main/Send.java
@@ -0,0 +1,5 @@
+package by.pvt.epam.main;
+
+public class Send {
+
+}
--- ZemliakovParsing/src/by/pvt/epam/factory/Garden.java
+++ ZemliakovParsing/src/by/pvt/epam/factory/Garden.java
@@ -5,7 +5,7 @@ public class Garden {
 	public static void main(String[] args) {
 
 		PlantBuilderFactory sFactory = new PlantBuilderFactory();
-		AbstractPlantsBuilder builder = sFactory.createPlantBuilder("sax");
+		AbstractPlantsBuilder builder = sFactory.createPlantBuilder("stax");
 		builder.buildSetPlants("garden.xml");
 		System.out.println(builder.getPlants());
 	}
