9afbca53b8a363fac6d81dec36dec4a7c8b28ed8
Nelegalos <zemliakov@gmail.com>
2014-09-08 16:09:52 +0300

    threads

---
+++ ZemliakovThreads/src/by/pvt/resource/exception/OperatorTechnicalException.java
@@ -0,0 +1,23 @@
+package by.pvt.resource.exception;
+
+@SuppressWarnings("serial")
+public class OperatorTechnicalException extends Exception {
+
+	
+
+	public OperatorTechnicalException() {
+		super();
+	}
+
+	public OperatorTechnicalException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public OperatorTechnicalException(String message) {
+		super(message);
+	}
+
+	public OperatorTechnicalException(Throwable cause) {
+		super(cause);
+	}
+}
